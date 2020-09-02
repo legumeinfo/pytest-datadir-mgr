95ca0360eba8f2a76d959924f2987d879423f156
Joel Berendzen <joel@generisbio.com>
2020-09-02 16:34:50 -0600

    deps

--- noxfile.py
+++ noxfile.py
@@ -32,7 +32,7 @@ def install_with_constraints(session, *args, **kwargs):
 @nox.session(python=["3.8"])
 def tests(session):
     """Run tests with pytest and pytest-cov."""
-    args = session.posargs or ["--cov"]
+    args = session.posargs or ["--cov=pytest_datadir_mgr"]
     session.run("poetry", "install", "--no-dev", external=True)
     install_with_constraints(session, "coverage[toml]", "pytest", "pytest-cov")
     session.run("pytest", *args)
